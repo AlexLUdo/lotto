86fb33e2b48b4aca3edb039089adaf0865fcf8c0
Aleksei <AlexLUdo@users.noreply.github.com>
2019-12-09 22:54:03 +0300

    NewVersion HomeWork 10

--- lotto_3.py
+++ lotto_3.py
@@ -1,144 +1,93 @@
 import random
-import sys
-import time
+barrels = 90  #бочонков
+rows = 3      #строк в карточке
+cols = 5      #цифр в строке
+posits = 9    #всего позиций
+gamers = [['Вундеркинд', False], ['Железяка', True], ['Железяка 2', True]]
 
-class Cask:
-    #достаем боченки по одному
-    def f(self):
-        lst = [x for x in range(1, self.amount + 1)]
-        random.shuffle(lst)
-        for i, y in enumerate(lst):
-            print('{:#^30}'.format('#'))
-            print('Новый бочонок: {} (осталось {})'.format(y, self.amount - (i+1)))
-            yield y
+class Card:
+    def __init__(self, gamer):
+        self.positions = []     
+        self.numbers = random.sample(range(1, barrels+1), rows * cols)
+        for i in range(rows):
+            pos = random.sample(range(posits),cols)
+            bpos = [1 if position in pos else 0 for position in range(posits)]
+            self.positions.append(bpos)
+        self.used = []
+        self.gamer = gamer
 
-    def __init__(self, amount):
-        self.amount = amount
-        self.gen = self.f()
+    def show(self):
+        print('Карточка: ', self.gamer[0])
+        print('#'*(posits*3-1))
+        for i in range(0, rows):
+            string = ''
+            itr = iter(sorted(self.numbers[i*cols : (i+1)*cols]))
 
-class Loto:
-    #шаманим карточки
-    def __set_card(self):
-        num = set()
-        while len(num) < self.all_row * 5:
-            num.add(random.randint(1, 90))
-        cards = list(num)
-        random.shuffle(cards)
-        #print(cards)
+            for j in range(0, posits):
+                if self.positions[i][j]:
+                    next_num = next(itr)
+                    string += '$$ ' if next_num in self.used else '{0:2d}'.format(next_num)+' '
+                else:
+                    string += '   '
+            print(string)
+        print('#' * (posits*3-1))
 
-        while len(cards) % self.all_row != 0:
-            cards.append('None')
-        self.all_row = int(len(cards) / self.all_row)
-        cards = [cards[i: i + self.all_row] for i in range(0,len(cards),self.all_row)]
+    def decision(self, bar_num, rest_num):
+        if self.gamer[1]:
 
-        for i in range(len(cards)):
-            cards[i].sort()
-        self.card_user = cards[:3]
-        self.card_comp = cards[3:]
-        self.card_smart = cards[2:5]
-        self.card_ufo = cards[1:15]
+            ans = bar_num in self.numbers
+        else:
+            print('Вынули бочонок: ', bar_num, ' (В мешке:', rest_num,')')
+            ans =  input('Зачеркнуть цифру? (у/n) ') in 'ДдYy'
+        return ans
 
-    def __init__(self, amount_card):
-        row = 3
-        self.all_row = row * amount_card
-        self.__set_card()
-
-
-    def get_card(self, card_player):
-        print('{:*^25}'.format(self.name))
-        print('{0[0]:>2} {0[1]:<10} {0[2]:<5} {0[3]} {0[4]} '.format(card_player[0]))
-        print('{0[0]:>4} {0[1]:<6} {0[2]:<4} {0[3]:<4} {0[4]} '.format(card_player[1]))
-        print('{0[0]} {0[1]:<5} {0[2]:<5} {0[3]:<5} {0[4]} '.format(card_player[2]))
-        print('{:*^25}'.format( '*'))
-
-    #Поиск номера на карточке и "поиск" победителя
-    def search(self, card_player, num_cask):
-        for i, n in enumerate(card_player):
-            if num_cask in n:
-                card_player[i][n.index(num_cask)] = '$'
-                self.score += 1
-                if self.score == 15:
-                    print('{} Победил!'.format(self.name))
-                    sys.exit(1)
-                return True
-        return False
-
-
-
-
-class Player(Loto):
-
-    def __init__(self, name):
-        self.name = name
-        self.score = 0
-
-def main():
-    players = {}
-    if __name__ == '__main__':
-
-      ncomp = 1
-      num_users = 1
-      num_players = int(input('СКОЛЬКО ИГРОКОВ БУДЕТ ИГРАТЬ? : '))
-      #for nconp in range (num_comp):
-      #    name_comp =input(f'Введите имя компа/смарфона {ncomp} : ')
-      num_users = int(input('СКОЛЬКО из них ГУМАНОИДОВ? : '))
-      #for nuser in range (num_users):
-      # name_user =input(f'Введите имя ГУМАНОИДА {nuser} : ')
-
-
-    def gen_play(num_comp, num_user):
-        for i in range(num_comp):
-            pl_name = f'comp-{i+1}'
-            self.players[pl_name] = Computer(pl_name)
-        for i in range(num_users):
-            pl_name = f'user-{i+1}'
-            self.players[pl_name] = User(pl_name)
-
-    game = Loto(4)
-    cask = Cask(90)
-#    if  num_players == 1 :
-    player1 = Player('Ваша карточка')
-    if num_players > 1:
-        player2  = Player('Карточка Компьютера')
-        if num_players > 2:
-            player3 = Player('Карточка Смартфона')
-            if num_players > 3:
-                player4 = Player('Карточка UFO')
-
-    while True:
-        num_cask = next(cask.gen)
-        player1.get_card(game.card_user)
-        if num_players > 1:
-            player2.get_card(game.card_comp)
-            if num_players > 2:
-                player3.get_card(game.card_smart)
-                if num_players > 3:
-                    player4.get_card(game.card_ufo)
-
-        inp_user = input('Зачеркнуть цифру? (y/n)')
-        if inp_user == 'y':
-            if player1.search(game.card_user, num_cask):
-                continue
+    def cover(self, bar_num, decis):
+        ans = (True, '')
+        if bar_num in self.numbers:
+            if decis:
+                self.used.append(bar_num)
+                if len(self.used) == rows * cols:
+                    ans = (False, self.gamer[0]+' Ура! Все номера закрыты!')
             else:
-                print('Game Over')
-                sys.exit(1)
-        if inp_user == 'n':
-            if player1.search(game.card_user, num_cask):
-                print('Game Over')
-                sys.exit(1)
-                if num_players <= 2:
-                    player2.search(game.card_comp, num_cask)
-                    if num_players <= 3:
-                        player3.search(game.card_comp, num_cask)
-                        if num_players <= 4:
-                            player4.search(game.card_comp, num_cask)
-                continue
+                ans = (False, self.gamer[0]+' :(((! Пропущен номер на карточке!')
+        else:
+            if decis:
+                ans = (False,  self.gamer[0]+' Ж(((! Нет такого номера :(((!')
+        return ans
 
-        if inp_user != 'n' and inp_user != 'y':
-            print('Введите y or n')
-            time.sleep(1)
-            continue
+class Bag:
+    def __init__(self):
+        self.cur_pos = -1
+        self.bag = random.sample(range(1, barrels + 1), barrels)
+
+    def next_barrel(self):
+        self.cur_pos += 1
+        if self.cur_pos >= barrels:
+            raise Exception('Бочонки RUN OUT')
+        return self.bag[self.cur_pos], barrels-self.cur_pos-1
+
+def new_game():
+    games = []
+    for gamer in gamers:
+        games.append(Card(gamer))
+    print('New game>><<>>>')
+    loop = True
+    bag = Bag()
+    msg = ''
+    while loop:
+        barrel, rest_num = bag.next_barrel()
+        for game in games:
+            game.show()
+            des = game.decision(barrel, rest_num)
+            answer = game.cover(barrel, des)
+            loop = answer[0]
+            msg = answer[1]
+            if not loop:
+                break
+        if rest_num == 0:
+            loop = False
+            msg = 'GAME OVER!'
+    print(msg)
+new_game()
 
 
-if __name__ == '__main__':
-    main()
---
+++ test_lotto3.py
@@ -0,0 +1,52 @@
+import pytest
+import unittest
+import random
+from lotto_3 import Card, Bag
+
+class TestCard(unittest.TestCase):
+
+    def setUp(self):
+        self.card = Card(['Железяка', True])
+
+    def tearDown(self):
+        pass
+
+    def test_init(self):
+        self.assertEqual(len(self.card.numbers), 15)
+        self.assertEqual(len(self.card.positions), 3)
+        self.assertEqual(len(self.card.used), 0)
+
+    def test_decision(self):
+        a = random.randint(0,90)
+        self.assertEqual(self.card.decision(a, 1), a in self.card.numbers)
+
+    def test_show(self):
+        self.assertIsNone(self.card.show())
+
+    def test_cover(self):
+        a = self.card.numbers[-1]
+        self.assertTrue(self.card.cover(a, True)[0])
+        self.assertFalse(self.card.cover(a, False)[0])
+
+class TestBag(unittest.TestCase):
+
+
+    def setUp(self):
+        self.bag = Bag()
+
+    def test_init(self):
+        self.assertEqual(len(self.bag.bag), 90)
+
+    def test_next_barrel(self):
+        self.assertNotEqual(self.bag.bag[0], self.bag.bag[-1])
+        while self.bag.cur_pos < 89:
+            self.assertNotEqual(self.bag.next_barrel(), self.bag.next_barrel())
+        with pytest.raises(Exception, match='Бочонки RUN OUT'):
+            self.bag.next_barrel()
+        try:
+            self.bag.next_barrel()
+        except:
+            self.assertRaises(Exception)
+
+if __name__ == '__main__':
+    unittest.main()
